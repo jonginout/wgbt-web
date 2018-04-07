e952dcb40e426fff2050d04d761c04640e4934d3
jonginout <jonginout@naver.com>
2018-04-07 09:44:43 +0900

    메인 정리

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <main-header></main-header>
 
-    <router-view></router-view>
+    <div id="main_view">
+      <router-view></router-view>
+    </div>
 
     <main-footer></main-footer>
   </div>
@@ -20,4 +22,9 @@
 <style lang="scss">
   @import url('http://fonts.googleapis.com/earlyaccess/notosanskr.css');
   @import './style/style.scss';
+
+  #main_view{
+    margin: 80px 0px 10px 0px;
+    padding: 0px 50px;
+  }
 </style>
